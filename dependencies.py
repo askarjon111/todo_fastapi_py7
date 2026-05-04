e2009a7b14e60f033d0f27ae33de036fa369e9b2
ASKAR <askarjon.abdullayev@gmail.com>
2026-05-04 20:51:40 +0500

    user roles

--- dependencies.py
+++ dependencies.py
@@ -35,8 +35,8 @@ async def get_current_user(token: str = Depends(oauth2_scheme), db: Session = De
     return user
 
 
-def role_checker(*allowed_roles: Roles):
-    def checker(user: User = Depends(get_current_user)):
+async def role_checker(*allowed_roles: Roles):
+    async def checker(user: User = Depends(get_current_user)):
         if user.role not in allowed_roles:
             raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Ruxsat etilmagan")
         return user
